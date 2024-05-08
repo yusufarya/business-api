9f9d9cdf6f8023bd78bc14c3c9d45310731dbd3d
yusufarya <aryaherby29nov2k@gmail.com>
2024-05-09 02:51:59 +0700

    add helpers

---
+++ src/utils/helper.ts
@@ -0,0 +1,16 @@
+export class Helper {
+    static dateLocal(date : Date) {
+        const utcTime = date.getTime();
+
+        // Jakarta time zone offset in milliseconds (7 hours ahead of UTC)
+        const jakartaOffset = 7 * 60 * 60 * 1000;
+
+        // Convert the UTC time to Jakarta time
+        const jakartaTime = utcTime + jakartaOffset;
+
+        // Create a new Date object representing the Jakarta time
+        const jakartaDate = new Date(jakartaTime);
+
+        return jakartaDate
+    }
+}

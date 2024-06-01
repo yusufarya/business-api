06dbad6484970ce5fe333f45a9c538acd3474507
yusufarya <aryaherby29nov2k@gmail.com>
2024-06-02 02:29:57 +0700

    upload image product

---
+++ src/middleware/upload-image.ts
@@ -0,0 +1,30 @@
+// middleware/upload.ts
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import fs from 'fs';
+
+export class UploadImage {
+    static uploadSingleProduct() {
+        const storage = multer.diskStorage({
+            destination: async (req, file, cb) => {
+                let dir;
+                if (process.env.NODE_ENV === 'production') {
+                    dir = path.join(__dirname, "../../dist/public/images/product");
+                } else {
+                    dir = path.join(__dirname, "../../src/public/images/product");
+                }
+                // Ensure directory exists
+                await fs.promises.mkdir(dir, { recursive: true });
+                cb(null, dir);
+            },
+            filename: (req, file, cb) => {
+                const uniqId = uuidv4();
+                cb(null, uniqId + "_" + file.originalname);
+            },
+        });
+
+        const upload = multer({ storage });
+        return upload.single("file");
+    }
+}
